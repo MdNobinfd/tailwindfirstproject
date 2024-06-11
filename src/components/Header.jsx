312fd12992809dc1b4615fc67896bc3fb62dd2cb
Mdnobinfd <mdnobin.fdr@gmail.com>
2024-06-11 21:17:41 +0600

    tailwind

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <header className='container container-sm mx-auto'>
+        <h2>Hello</h2>
+    </header>
+    </>
+  )
+}
+
+export default Header
